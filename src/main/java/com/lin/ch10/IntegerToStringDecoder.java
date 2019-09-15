8975742da7c7fcd7841c3e795e3ba39eaffdaf28
lkmc2 <lkmc2@163.com>
2019-09-15 16:04:46 +0800

    添加将整数转换成字符串的解码器（使用 MessageToMessageDecoder）

---
+++ src/main/java/com/lin/ch10/IntegerToStringDecoder.java
@@ -0,0 +1,23 @@
+package com.lin.ch10;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * 将整数转换成字符串的解码器（使用 MessageToMessageDecoder）
+ *
+ * 拓展了 MessageToMessageDecoder ，以将一种 POJO 转换成另一种 POJO
+ * @author lkmc2
+ * @date 2019/9/15 15:54
+ */
+public class IntegerToStringDecoder extends MessageToMessageDecoder<Integer> {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, Integer msg, List<Object> outList) throws Exception {
+        // 将传入的整数转换成字符串后，添加到输出列表中
+        outList.add(String.valueOf(msg));
+    }
+
+}
---
+++ src/test/java/com/lin/ch10/IntegerToStringDecoderTest.java
@@ -0,0 +1,32 @@
+package com.lin.ch10;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 将整数转换成字符串的解码器（使用 MessageToMessageDecoder）测试
+ * @author lkmc2
+ * @date 2019/9/15 15:56
+ */
+public class IntegerToStringDecoderTest {
+
+    @Test
+    public void testDecode() {
+        // 创建一个 EmbeddedChannel，并添加一个 IntegerToStringDecoder
+        EmbeddedChannel channel = new EmbeddedChannel(new IntegerToStringDecoder());
+
+        // 将数据写入 EmbeddedChannel
+        assertTrue(channel.writeInbound(99));
+
+        // 标记 Channel 为已完成状态
+        assertTrue(channel.finish());
+
+        // 读取所生成的消息
+        String read = channel.readInbound();
+        assertEquals("99", read);
+    }
+
+}
