764f483e5a36e3f9240d2fdf53e1f87a6f529ca9
lkmc2 <lkmc2@163.com>
2019-09-15 13:22:33 +0800

    添加 EmbeddedChannel 测试

---
+++ src/main/java/com/lin/ch09/FixedLengthFrameDecoder.java
@@ -0,0 +1,37 @@
+package com.lin.ch09;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * 固定长度的栈帧解码器
+ * @author lkmc2
+ * @date 2019/9/15 12:15
+ */
+public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
+
+    /** 栈帧长度 **/
+    private final int frameLength;
+
+    public FixedLengthFrameDecoder(int frameLength) throws IllegalAccessException {
+        if (frameLength <= 0) {
+            throw new IllegalAccessException("frameLength must be a positive integer: " + frameLength);
+        }
+        this.frameLength = frameLength;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> outList) throws Exception {
+        // 当可读字节长度大于等于栈帧长度时
+        while (in.readableBytes() >= frameLength) {
+            // 读取一个栈帧长度的字节信息
+            ByteBuf buf = in.readBytes(frameLength);
+            // 放入字节信息到输出列表中
+            outList.add(buf);
+        }
+    }
+
+}
---
+++ src/test/java/com/lin/ch09/FixedLengthFrameDecoderTest.java
@@ -0,0 +1,57 @@
+package com.lin.ch09;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * EmbeddedChannel 测试
+ * @author lkmc2
+ * @date 2019/9/15 13:09
+ */
+public class FixedLengthFrameDecoderTest {
+
+    @Test
+    public void testFramesDecoded() throws IllegalAccessException {
+        // 创建（获取）堆中的缓存字节
+        ByteBuf buf = Unpooled.buffer();
+
+        // 存储 9 字节的数据
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+
+        // 拷贝字节数据（浅拷贝，修改该变量可能的原来的字节数据产生影响）
+        ByteBuf input = buf.duplicate();
+
+        // 创建一个 EmbeddedChannel，并添加一个 FixedLengthFrameDecoder，其将以 3 字节的帧长度被测试
+        EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+
+        // 将数据写入 EmbeddedChannel
+        assertTrue(channel.writeInbound(input.retain()));
+
+        // 标记 Channel 为已完成状态
+        assertTrue(channel.finish());
+
+        // 读取所生成的消息，并且验证是否有 3 帧（切片），其中每帧（切片）都为 3 字节
+        ByteBuf read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        // 上面已经读取完 9 个字节，所以这里没有字节可以读取
+        assertNull(channel.readInbound());
+        buf.release();
+    }
+
+}
